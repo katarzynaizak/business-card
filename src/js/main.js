f30a2789d94b3311cb68b0b7ccc0941e9122e258
Kasia Izak <kontakt@kasiaizak.pl>
2019-10-07 11:00:01 +0200

    remove moment and add browser-update package

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,11 @@
 "use strict";
 
-import moment from 'moment';
+import browserUpdate from 'browser-update';
 
-console.log(moment().format('MMMM Do YYYY, h:mm:ss a'))
+browserUpdate({
+  required: {e:11,f:-6,o:-6,s:10,c:-6},
+  insecure:true,
+  unsupported:true,
+  reminder:0,
+  reminderClosed: 1
+});
